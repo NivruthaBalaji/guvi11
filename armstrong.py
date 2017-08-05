b4868f315470749af2dd9d1f0755194fd743c1ec
NivruthaBalaji <30365814+NivruthaBalaji@users.noreply.github.com>
2017-08-05 11:04:50 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+num = int(raw_input("Enter the number : "))
+temp = num
+temp2 = num
+l=0
+count = 0
+while(temp2 > 0):
+    temp2 = int(temp2 / 10)
+    count += 1
+while(num > 0):
+    z = num % 10
+    num = int(num/10)
+    l=l+z**count
+if(temp == l):
+    print("Armstrong Number")
+else:
+    print("Not an armstrong number")
